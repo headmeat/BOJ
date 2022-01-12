ee1a8d14b76b74569ae6f7258f66fa99e27b2c23
headmeat <headmeat@naver.com>
2022-01-13 07:35:33 +0900

    Update 연산자 끼워넣기.py

--- 실버/연산자 끼워넣기.py
+++ 실버/연산자 끼워넣기.py
@@ -1,26 +1,20 @@
+from itertools import permutations
+
 N = int(input())
 lst = list(map(int, input().split()))
 comp = list(map(int, input().split()))
 res = []
+st = set()
 
-def iter(N, arr):
-    if len(arr) == N:
-        sm = lst[0]
-        print(arr)
-        for i in range(len(arr)):
-            if calcs[arr[i]] == "+": sm = sm + lst[i+1]
-            elif calcs[arr[i]] == "-": sm = sm - lst[i+1]
-            elif calcs[arr[i]] == "*": sm = sm * lst[i+1]
-            elif calcs[arr[i]] == "/": sm = int(sm / lst[i+1])
-        res.append(sm)
+def calculate(comp):
+    sm = lst[0]
 
-        return
-
-    for i in range(N):
-        if i not in arr:
-            arr.append(i)
-            iter(N, arr)
-            arr.remove(i)
+    for i in range(len(comp)):
+        if comp[i] == "+": sm = sm + lst[i+1]
+        elif comp[i] == "-": sm = sm - lst[i+1]
+        elif comp[i] == "*": sm = sm * lst[i+1]
+        elif comp[i] == "/": sm = int(sm / lst[i+1])
+    res.append(sm)
 
 calcs = []
 
@@ -30,8 +24,8 @@ for i in range(len(comp)):
         elif i == 1: calcs.append("-")
         elif i == 2: calcs.append("*")
         else: calcs.append("/")
-
-iter(N-1, [])
-#print(res)
+c = 0
+for i in set(permutations(calcs, len(calcs))):
+    calculate(i)
 print(max(res))
 print(min(res))
