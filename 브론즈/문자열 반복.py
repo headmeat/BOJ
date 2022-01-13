8e1ddcfe363ada7a8e73be110b49c2a1a68fa412
headmeat <headmeat@naver.com>
2022-01-14 00:45:15 +0900

    Update 문자열 반복.py

--- 브론즈/문자열 반복.py
+++ 브론즈/문자열 반복.py
@@ -1,1 +1,20 @@
+N = int(input())
 
+def iter(N, M, arr):
+    if len(arr) == M:
+        print(" ".join(map(str, arr)))
+        return
+
+    for i in range(1, N+1):
+        if i not in arr:
+            arr.append(i)
+            iter(N, M, arr)
+            arr.remove(i)
+
+for i in range(N):
+    L, S = input().split()
+    L = int(L)
+
+    for j in range(len(S)):
+        print(S[j] * L, end = "")
+    print()
