9cb590f7f0c23710f7a5b18bfd807d46e4be4654
headmeat <headmeat@naver.com>
2022-01-14 06:12:36 +0900

    Update 피보나치 수 5.py

--- 브론즈/피보나치 수 5.py
+++ 브론즈/피보나치 수 5.py
@@ -12,3 +12,24 @@ def fibonacci(N, arr):
     return fibonacci(N-2, arr) + fibonacci(N-1, arr)
 
 print(fibonacci(N, arr))
+
+#dp
+"""
+N = int(input())
+
+fibo = [0] * (N+1)
+
+#N은 원소 번호: #1~#N
+def fibonacci(N):
+    if N <= 1: return N
+    
+    fibo[0] = 0
+    fibo[1] = 1
+
+    for i in range(2, N+1):
+        fibo[i] = fibo[i-1] + fibo[i-2]
+    
+    return fibo[N]
+
+print(fibonacci(N))
+"""
