b12a9db3c0939eb6fa28207cfc86d0077f780b0b
headmeat <headmeat@naver.com>
2022-01-21 20:18:56 +0900

    Create 2차원 배열의 합.py

---
+++ 브론즈/2차원 배열의 합.py
@@ -0,0 +1,22 @@
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for i in range(N)]
+
+sm = 0
+noo = []
+
+for i in range(len(arr[0])):
+    for j in range(len(arr)):
+        sm += arr[j][i]
+        noo.append(sm)
+
+T = int(input())
+
+idxs = [list(map(lambda x: x-1, map(int, input().split()))) for i in range(T)]
+
+for idx in idxs:
+    i, j, x, y = map(int, idx)
+
+    print("I,J", (i*len(arr[0])) + (j))
+    print("X,Y", (x*len(arr[0])) + (y))
+    print("SQR1,2", noo[(x*len(arr[0])) + y], noo[(i*len(arr[0])) + j])
+    print(noo[(x*len(arr[0])) + y] - noo[(i*len(arr[0])) + j])
