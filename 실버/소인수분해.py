58edb8bb5538a3250ac3c7870e3dd03308cb5413
headmeat <headmeat@naver.com>
2022-01-24 05:03:16 +0900

    Update 소인수분해.py

--- 실버/소인수분해.py
+++ 실버/소인수분해.py
@@ -4,6 +4,28 @@ lst = [0 for i in range(N+1)]
 lst[0] = lst[1] = -1 #0과 1은 소수에서 제외
 primes = []
 
+i = 2
+mx = int(N ** 0.5) + 1
+
+while N != 1:
+    if N % i == 0:
+        print(i)
+        N = int(N / i)
+        i = 2
+    else:
+        if i < mx: i += 1
+        else: 
+            print(N)
+            break
+
+"""
+#초기 풀이
+N = int(input())
+
+lst = [0 for i in range(N+1)]
+lst[0] = lst[1] = -1 #0과 1은 소수에서 제외
+primes = []
+
 for i in range(2, int(N ** 0.5) + 1):
     for j in range(2*i, N+1, i):
         lst[j] = -1
@@ -19,3 +41,4 @@ while(N != 1):
         N /= primes[idx]
         idx = 0
     else: idx += 1
+"""
