149292de4e30bf3898a0cdb347720fe81b1a42ee
headmeat <headmeat@naver.com>
2022-01-24 03:22:01 +0900

    Update 2차원 배열의 합.py

--- 브론즈/2차원 배열의 합.py
+++ 브론즈/2차원 배열의 합.py
@@ -1,5 +1,26 @@
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for i in range(N)]
+T = int(input())
+idxs = [list(map(int, input().split())) for i in range(T)]
+noo = [[0 for i in range(M + 1)] for i in range(N + 1)]
+
+for i in range(1, len(noo)):
+    noo[i][1] = arr[i-1][0]
+    for j in range(2, len(noo[0])):
+        noo[i][j] = noo[i][j-1] + arr[i-1][j-1]
+
+for i in range(1, len(noo)):
+    for j in range(len(noo[0])):
+        noo[i][j] += noo[i-1][j]
+
+for idx in idxs:
+    i, j, x, y = map(int, idx)
+    print(noo[x][y] - noo[i-1][y] - noo[x][j-1] + noo[i-1][j-1])
+
+#아래는 첫 풀이. 복기용으로 남김.    
+"""
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for i in range(N)]
 
 sm = 0
 noo = []
@@ -20,4 +41,4 @@ for idx in idxs:
     print("X,Y", (x*len(arr[0])) + (y))
     print("SQR1,2", noo[(x*len(arr[0])) + y], noo[(i*len(arr[0])) + j])
     print(noo[(x*len(arr[0])) + y] - noo[(i*len(arr[0])) + j])
-#
+"""
