632ae74c458771aefec837a1d292cd4cb265fdb9
headmeat <headmeat@naver.com>
2022-01-24 23:53:06 +0900

    Update 대회 or 인턴.py

--- 브론즈/대회 or 인턴.py
+++ 브론즈/대회 or 인턴.py
@@ -14,3 +14,26 @@ else:
         if (K % 3) == 0: break
         K += 1
     print(int(M) - int(K/3))
+
+"""
+#새로운 풀이
+N, M, K = map(int, input().split())
+tmp = K
+
+if N % 2 != 0: 
+    N -= 1
+    K -= 1
+
+if N < 2 * M:
+    K -= M - int(N / 2)
+    M = int(N / 2)
+elif N > 2 * M:
+    K -= N - (2 * M)
+    N = 2 * M
+
+if K > 0:
+    M -= K // 3
+    if K % 3 != 0: M -=1
+
+print(M)
+"""
