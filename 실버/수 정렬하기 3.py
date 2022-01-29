59e83c24998d42004b6645d5c2fdc13319ecfe7b
최재용 <headmeat@naver.com>
2022-01-30 02:48:36 +0900

    Create 수 정렬하기 3.py

---
+++ 실버/수 정렬하기 3.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(input())
+arr = [0 for i in range(10001)]
+
+for i in range(N):
+    arr[int(sys.stdin.readline())] += 1
+cnt = 0
+
+for i in range(1, 10001):
+    if cnt == N: break
+    if arr[i] >= 1: 
+        #print(f"{i}\n"*arr[i], end="")
+        for j in range(arr[i]): sys.stdout.write("%d\n" % (i))
