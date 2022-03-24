798cbb60fac146c62f273c8c82d5094c5ee53274
최재용 <headmeat@naver.com>
2022-03-25 03:22:15 +0900

    Create 팰린드롬 분할.py

---
+++ 골드/팰린드롬 분할.py
@@ -0,0 +1,26 @@
+from sys import stdin
+input = stdin.readline
+
+arr = [x for x in input().rstrip()]
+n = len(arr)
+dp = [[0 for _ in range(n)] for _ in range(n)]
+pal = [10**15 for _ in range(n)]
+
+for i in range(n):
+    dp[i][i] = 1
+    if i+1<n and arr[i] == arr[i+1]: dp[i][i+1] = 1
+
+for i in range(2, n):
+    for j in range(n):
+        if j+i<n:
+            if dp[j+1][j+i-1] == 1 and arr[j] == arr[j+i]:
+                dp[j][j+i] = 1
+        else: break
+
+for k in range(n):
+    for i in range(k+1):
+        if dp[i][k]==1:
+            if i-1>=0: pal[k]=min(pal[k], pal[i-1]+1)
+            else: pal[k] = 1
+
+print(pal[-1])
