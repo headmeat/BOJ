2bea9ebf3015e02f103a143c6fdc31d0ea9265a9
최재용 <headmeat@naver.com>
2022-03-28 05:27:16 +0900

    Create 가장 긴 바이토닉 부분 수열.py

---
+++ 골드/가장 긴 바이토닉 부분 수열.py
@@ -0,0 +1,24 @@
+from sys import stdin
+input = stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+lis = [1 for _ in range(n)]
+lds = [1 for _ in range(n)]
+
+for i in range(n):
+    for j in range(i):
+        if arr[i]>arr[j]:
+            lis[i] = max(lis[i], lis[j] + 1)
+
+for i in range(n-1, -1, -1):
+    for j in range(i+1, n):
+        if arr[i]>arr[j]:
+            lds[i] = max(lds[i], lds[j]+1)
+
+mx = 0
+
+for i in range(n):
+    mx = max(mx, lds[i]+lis[i]-1)
+
+print(mx)
