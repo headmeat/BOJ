82128e422450a3443d2dc16d390373bf6ca9e650
최재용 <headmeat@naver.com>
2022-03-29 08:44:43 +0900

    Create 이친수.py

---
+++ 실버/이친수.py
@@ -0,0 +1,13 @@
+from sys import stdin
+input = stdin.readline
+
+n = int(input())
+
+dp = [0 for _ in range(91)]
+
+dp[1] = dp[2] = 1
+
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])
