54f6ce19363498b93528b7b8ae354e86e32aced5
최재용 <headmeat@naver.com>
2022-06-10 01:30:29 +0900

    Update 암호 만들기.py

--- 골드/암호 만들기.py
+++ 골드/암호 만들기.py
@@ -29,3 +29,31 @@ def solve(i, arr):
     solve(i+1, arr)
 
 solve(0, [0 for _ in range(len(string))])
+
+"""0610-개선
+from sys import stdin
+input = stdin.readline
+
+l, c = map(int, input().split())
+keys = input().rstrip().split()
+keys.sort()
+nouns = ['a', 'e', 'i', 'o', 'u']
+
+def solve(l, arr, idx):
+    global keys, nouns
+    if len(arr)==l:
+        n = 0
+
+        for k in nouns:
+            if k in arr: n += 1
+
+        if len(arr) - n>=2 and n>0: print("".join(arr))
+        return
+
+    for i in range(idx, len(keys)):
+        arr.append(keys[i])
+        solve(l, arr, i+1)
+        del arr[-1]
+
+solve(l, [], 0)
+"""
